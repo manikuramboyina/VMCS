1f789d30f350fb1ba568bdbb4324c82a3a9b6735
unknown <A0119930E@BOR2602.iss.nus.edu.sg>
2015-05-09 13:28:47 +0800

    commit message

---
+++ output/VMCS_PaymentByCardWithPin/VMCS Sources/src/sg/edu/nus/iss/vmcs/store/Card.java
@@ -0,0 +1,9 @@
+package sg.edu.nus.iss.vmcs.store;
+
+public class Card extends StoreObject {
+private int number;
+private double balance;
+
+public Card() {}public void method1() {
+System.out.println("Test");
+}}
